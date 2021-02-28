11f0ea27aa42206499b79912b78ccfdd86eda769
o2sevruk <o2sevruk@gmail.com>
2021-02-28 20:47:48 +0300

    Environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,76 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
-require('@babel/polyfill');
+require("@babel/polyfill");
 
 module.exports = {
-	mode: 'development',
-	devtool: 'source-map',
-	entry: [
-		'@babel/polyfill',
-		'./src/js/index.js',
-		'./src/scss/styles.scss',
-	],
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, './dist'),
-	},
-	devServer: {
-		contentBase: path.join(__dirname, '/'),
-		compress: true,
-		port: 8080,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.(sa|sc|c)ss$/,
-				exclude: /(node_modules)/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'sass-loader',
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: 'styles.css',
-		}),
-		new WriteFilePlugin(),
-		new CopyPlugin({
-			patterns: [
-				{
-					from: '*css',
-					to: 'leaflet',
-					context: 'node_modules/leaflet/dist'
-				},
-				{
-					from: 'images/*',
-					to: 'leaflet',
-					context: 'node_modules/leaflet/dist'
-				},
-			],
-		}),
-	],
+  mode: "development",
+  devtool: "source-map",
+  entry: ["./src/js/index.js", "./src/scss/styles.scss"],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/"),
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new WriteFilePlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "*css",
+          to: "leaflet",
+          context: "node_modules/leaflet/dist",
+        },
+        {
+          from: "images/*",
+          to: "leaflet",
+          context: "node_modules/leaflet/dist",
+        },
+      ],
+    }),
+  ],
 };
